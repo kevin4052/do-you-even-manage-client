ba5bf3d835248c5913b04fb368374bd76f9c30ff
Kevin Hernandez <kevin.hernandez001@gmail.com>
2020-11-09 15:39:27 -0500

    added fontawesome and working on setting listItems to active

--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -3,6 +3,10 @@ import { Link } from 'react-router-dom';
 import AUTH_SERVICE from '../../services/AuthService';
 import TEAM_SERVICE from '../../services/TeamService';
 import TASK_SERVICE from '../../services/TaskService';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import { faTasks } from '@fortawesome/free-solid-svg-icons';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
 
 export default class SideNav extends Component {
     state = {
@@ -31,7 +35,21 @@ export default class SideNav extends Component {
             .logout()
             .then(() => this.props.onUserChange(null))
             .catch(err => console.log({ err }));
-    };
+    }
+
+    itemIsActive = (event) => {
+        let listClassName = event.target.className;
+        // let ulClassName = event.target.parentElement.children[1].className;
+
+        console.log( {listClassName} );
+        // console.log( {ulClassName} );
+
+        !listClassName.split(' ').includes('active')
+        ? listClassName += ' active'
+        : listClassName = listClassName.split(' ').filter(name => name !== 'active').join(' ');
+
+        event.target.className = listClassName;
+    }
 
     render() {
         console.log('side nav team props', this.props.userTeams)
@@ -43,33 +61,71 @@ export default class SideNav extends Component {
                     </Link>
                     <Link to='/home'>APP LOGO</Link>
                 </div>
+
                 <div className='sidenav-wrapper'>
+
                     <div className="user">
-                        <div className='user-info'>
+                        <div className='photo'>
                             <img src={this.props.currentUser?.profileImg} alt='profile img'/>
-                            <h3>{this.props.currentUser?.firstName}</h3>
                         </div>
-                        <button onClick={this.logoutAndLiftUserState}> Logout </button>
+                        <div className='user-info'>
+                            <div>
+                                <span>{this.props.currentUser?.firstName}</span>
+                                <div className='caret'>
+                                    <FontAwesomeIcon icon={faCaretDown} />
+                                </div>
+                            </div>
+                            <div className='collapse show'>
+                                <ul>
+                                    <li>
+                                        <button onClick={this.logoutAndLiftUserState}> Logout </button>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        
                     </div>
-                    <div className="nav">
-                        <h3>My Teams</h3>
+
+                    <ul className="nav">
+                        <li className='' onClick={this.itemIsActive}>
+                            <div className='list-info'  >
+                                <div className='icon'>
+                                    <FontAwesomeIcon icon={faUsers} />
+                                </div>
+                                <div>My Teams</div>
+                                <div className='caret'>
+                                    <FontAwesomeIcon icon={faCaretDown} />
+                                </div>
+                            </div>
                             <ul className='collapse'>
                                 {
                                     this.props.userTeams?.map(team =>
                                     <>
-                                        <li key={team._id}>{team.name}</li>
+                                        <li key={team._id} className=''>{team.name}
                                             <ul className='collapse'>
                                                 {
                                                     team.projects.map(project => 
                                                     <li key={project._id}>{project.name}</li>)
                                                 }
                                             </ul>
+                                        </li>
                                     </>
                                     )
                                 }
                             </ul>
-                        <h3>My Tasks</h3>
-                    </div>
+                        </li>
+                        <li className='' onClick={this.itemIsActive} >
+                            <div className='list-info' >
+                                <div className='icon'>
+                                    <FontAwesomeIcon icon={faTasks} />
+                                </div>
+                                <div className='list-info-title'>My Tasks</div>
+                                <div className='caret'>
+                                    <FontAwesomeIcon icon={faCaretDown} />
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         )
